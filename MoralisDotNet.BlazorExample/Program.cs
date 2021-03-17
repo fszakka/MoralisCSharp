2398ca5b2588c55a940eb1aa8ba9020eb7a5000f
Niclas Hallgren <contact@hallgrenstudios.com>
2021-03-17 21:36:37 +0100

    removed unused using statement

--- MoralisDotNet.BlazorExample/Program.cs
+++ MoralisDotNet.BlazorExample/Program.cs
@@ -1,17 +1,13 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using MoralisDotNet.BlazorUtils;
-using MoralisDotNet.Metamask;
-
-namespace MoralisDotNet.Blazor
+namespace MoralisDotNet.BlazorExample
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using BlazorUtils;
+    using Metamask;
+
     public class Program
     {
         public static async Task Main(string[] args)
